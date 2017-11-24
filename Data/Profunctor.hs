f2657750d70e0aec198477525d3f2636ee0b1819
M Farkas-Dyck <strake888@gmail.com>
2017-11-23 16:30:45 -0800

    costrong profunctors

--- Data/Profunctor.hs
+++ Data/Profunctor.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE MultiParamTypeClasses, FlexibleContexts #-}
+{-# LANGUAGE DefaultSignatures #-}
 
 module Data.Profunctor where
 
@@ -8,6 +9,8 @@ import Control.Applicative
 import Control.Arrow (Kleisli (..))
 import Control.Category
 import Control.Monad
+import Control.Monad.Fix
+import Data.Bifunctor.Braided
 
 class Profunctor p where
     dimap :: (a -> b) -> (c -> d) -> p b c -> p a d
@@ -48,6 +51,28 @@ instance Strong Either (->) where
 instance Monad m => Strong Either (Kleisli m) where
     strong (Kleisli f) (Kleisli g) = Kleisli $ \ case Left  x -> Left  <$> f x
                                                       Right y -> Right <$> g y
+
+class Profunctor p => Costrong f p where
+    costrongL :: p (f a c) (f b c) -> p a b
+    costrongR :: p (f a b) (f a c) -> p b c
+
+    default costrongL :: Braided f => p (f a c) (f b c) -> p a b
+    costrongL = costrongR . dimap braid braid
+
+    default costrongR :: Braided f => p (f a b) (f a c) -> p b c
+    costrongR = costrongL . dimap braid braid
+
+instance Costrong (,) (->) where
+    costrongL f a = let (b, c) = f (a, c) in b
+
+instance MonadFix m => Costrong (,) (Kleisli m) where
+    costrongL (Kleisli f) = Kleisli $ \ a -> fst <$> mfix (f . (,) a . snd)
+
+instance Costrong Either (->) where
+    costrongL f = let go = either id (go . f . Right) in go . f . Left
+
+instance Monad m => Costrong Either (Kleisli m) where
+    costrongL (Kleisli f) = let go = either pure (go <=< f . Right) in Kleisli (go <=< f . Left)
 
 class Profunctor p => Closed p where
     closed :: p a b -> p (c -> a) (c -> b)
