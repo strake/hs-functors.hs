cef3cc38e601d4f9dcb95143bb726c79e0df3396
M Farkas-Dyck <strake888@gmail.com>
2017-11-23 18:24:16 -0800

    (cata, ana)morphism

--- Data/Fix.hs
+++ Data/Fix.hs
@@ -2,6 +2,8 @@
 
 module Data.Fix where
 
+import Control.Comonad
+import Data.Cotraversable
 import Data.Function (on)
 import Data.Functor.Classes
 import Text.Read
@@ -15,3 +17,15 @@ instance Show1 f => Show (Fix f) where showsPrec n = showsPrec1 n . unFix
 
 mapFix :: Functor f => (∀ a . f a -> g a) -> Fix f -> Fix g
 mapFix f (Fix x) = Fix (f (mapFix f <$> x))
+
+cata :: Functor f => (f a -> a) -> Fix f -> a
+cata f (Fix x) = f (cata f <$> x)
+
+cataM :: (Traversable f, Monad m) => (f a -> m a) -> Fix f -> m a
+cataM f (Fix x) = traverse (cataM f) x >>= f
+
+ana :: Functor f => (a -> f a) -> a -> Fix f
+ana f x = Fix (ana f <$> f x)
+
+anaW :: (Cotraversable f, Comonad ɯ) => (ɯ a -> f a) -> ɯ a -> Fix f
+anaW f x = Fix (cotraverse (anaW f) (x =>> f))
