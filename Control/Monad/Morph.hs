b967db780ae8a820a3f3ccf29b4687358ae5d0af
M Farkas-Dyck <strake888@gmail.com>
2018-05-21 21:59:03 -0800

    more `MFunctor` and `MMonad` instances

--- Control/Monad/Morph.hs
+++ Control/Monad/Morph.hs
@@ -3,13 +3,16 @@
 module Control.Monad.Morph where
 
 import Control.Monad
+import Control.Monad.Trans.Accum
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Except
 import Control.Monad.Trans.Identity
 import Control.Monad.Trans.Maybe
 import Control.Monad.Trans.Reader
-import Control.Monad.Trans.State
-import Control.Monad.Trans.Writer
+import Control.Monad.Trans.State.Lazy as L
+import Control.Monad.Trans.State.Strict as S
+import Control.Monad.Trans.Writer.Lazy as L
+import Control.Monad.Trans.Writer.Strict as S
 import Data.Functor.Compose
 import Data.Functor.Product
 import Data.Functor.Sum
@@ -31,16 +34,21 @@ instance MFunctor (Sum f) where
     mmap _ (InL x) = InL x
     mmap f (InR y) = InR (f y)
 
-instance MFunctor (ExceptT e) where mmap = mapExceptT
-instance MFunctor IdentityT   where mmap = mapIdentityT
-instance MFunctor MaybeT      where mmap = mapMaybeT
-instance MFunctor (ReaderT r) where mmap = mapReaderT
-instance MFunctor (StateT  s) where mmap = mapStateT
-instance MFunctor (WriterT w) where mmap = mapWriterT
+instance MFunctor (ExceptT e)   where mmap = mapExceptT
+instance MFunctor IdentityT     where mmap = mapIdentityT
+instance MFunctor MaybeT        where mmap = mapMaybeT
+instance MFunctor (ReaderT r)   where mmap = mapReaderT
+instance MFunctor (L.StateT s)  where mmap = L.mapStateT
+instance MFunctor (S.StateT s)  where mmap = S.mapStateT
+instance MFunctor (L.WriterT w) where mmap = L.mapWriterT
+instance MFunctor (S.WriterT w) where mmap = S.mapWriterT
+instance MFunctor (AccumT c)    where mmap = mapAccumT
 
 instance MMonad (ExceptT e) where mjoin (ExceptT   (ExceptT   x)) = ExceptT (join <$> x)
 instance MMonad IdentityT   where mjoin (IdentityT (IdentityT x)) = IdentityT x
 instance MMonad MaybeT      where mjoin (MaybeT    (MaybeT    x)) = MaybeT (join <$> x)
 instance MMonad (ReaderT r) where mjoin (ReaderT f) = ReaderT (join (runReaderT . f))
-instance Monoid w => MMonad (WriterT w) where
-    mjoin (WriterT (WriterT x)) = WriterT ((\ ((a, u), v) -> (a, u <> v)) <$> x)
+instance Monoid w => MMonad (L.WriterT w) where
+    mjoin (L.WriterT (L.WriterT x)) = L.WriterT ((\ ((a, u), v) -> (a, u <> v)) <$> x)
+instance Monoid w => MMonad (S.WriterT w) where
+    mjoin (S.WriterT (S.WriterT x)) = S.WriterT ((\ ((a, u), v) -> (a, u <> v)) <$> x)
